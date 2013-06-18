2d710a04b327d334d18e8fdf776b1d970ddf8642
alessandro.leite <alessandro.leite@alessandro.cc>
2013-06-18 17:08:53 +0200

    refactored to removed duplicated code

--- bin/mtranspose.c
+++ bin/mtranspose.c
@@ -10,10 +10,10 @@ int main(int argc, char **argv) {
 	long s = 0;
 
 	int *m;
-	int i, j;
+	int i, j, it;
 
-	if (argc < 3) {
-		printf("Usage <matrix size> <sleep time in seconds> \n");
+	if (argc < 4) {
+		printf("Usage <matrix size> <sleep time in seconds> <number of iteractions>\n");
 		exit(-1);
 	} else {
 		if (sscanf(argv[1], "%d", &n) != 1) {
@@ -22,7 +22,12 @@ int main(int argc, char **argv) {
 		}
 
 		if (sscanf(argv[2], "%d", &t) != 1) {
-			printf("Sleep size scanf failed.\n");
+			printf("Sleep time scanf failed.\n");
+			exit(-1);
+		}
+		
+		if (sscanf(argv[3], "%d", &it) != 1) {
+			printf("Iterations scanf failed.\n");
 			exit(-1);
 		}
 	}
@@ -30,17 +35,30 @@ int main(int argc, char **argv) {
 	m = malloc(n * n * sizeof(int));	
 
 	for (i = 0; i < n; i++)
+	{
 		for (j = 0; j < n; j++)
+		{
 			m[i * n + j] = rand();
+		}
+	}
+		
 
 	printf("Going to sleep\n");
 	sleep(t);
 	printf("I am awake\n");
 
-	for (i = 0; i < n; i++)
-		for (j = 0; j < n; j++)
-			s += m[i * n + j];
-
+    while (it > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				s += m[i * n + j];
+			}
+		}
+		it--;
+	}
+	
 	printf("S = %lu\n", s);
 	free(m);
 }
